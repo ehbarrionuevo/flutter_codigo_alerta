bfcb6cd94be86015e2b4e8aefe5cf049017e0171
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-08-20 09:17:32 -0500

    Mods 1.7

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo_alerta/ui/widgets/background_widget.dart';
 import 'package:flutter_codigo_alerta/ui/widgets/general_widget.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,53 +9,13 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
+
 
     return Scaffold(
       body: Stack(
         children: [
           //Fondo
-
-          Stack(
-            clipBehavior: Clip.none,
-            children: [
-              Positioned(
-                top: -height * 0.18,
-                left: -height * 0.05,
-                child: Transform.rotate(
-                  angle: pi / 4,
-                  child: Container(
-                    height: height * 0.6,
-                    width: height * 0.6,
-                    decoration: BoxDecoration(
-                      color: Colors.redAccent,
-                      borderRadius: BorderRadius.circular(height * 0.17),
-                      gradient: const LinearGradient(
-                        begin: Alignment.centerRight,
-                        end: Alignment.centerLeft,
-                        colors: [
-                          Color(0xffC064DD),
-                          Color(0xff5D28FE),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                top: -20,
-                left: -30,
-                child: circleWidget(90),
-              ),
-
-              Positioned(
-                top: 40,
-                right: -80,
-                child: circleWidget(100),
-              ),
-            ],
-          ),
-
+          BackgroundWidget(),
           //Contenido principal
 
           Text("Hola"),
---
+++ lib/ui/widgets/background_widget.dart
@@ -0,0 +1,55 @@
+
+
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:flutter_codigo_alerta/ui/widgets/general_widget.dart';
+
+class BackgroundWidget extends StatelessWidget {
+  const BackgroundWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    double height = MediaQuery.of(context).size.height;
+
+    return  Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Positioned(
+          top: -height * 0.18,
+          left: -height * 0.05,
+          child: Transform.rotate(
+            angle: pi / 4,
+            child: Container(
+              height: height * 0.6,
+              width: height * 0.6,
+              decoration: BoxDecoration(
+                color: Colors.redAccent,
+                borderRadius: BorderRadius.circular(height * 0.17),
+                gradient: const LinearGradient(
+                  begin: Alignment.centerRight,
+                  end: Alignment.centerLeft,
+                  colors: [
+                    Color(0xffC064DD),
+                    Color(0xff5D28FE),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: -20,
+          left: -30,
+          child: circleWidget(90),
+        ),
+
+        Positioned(
+          top: 40,
+          right: -80,
+          child: circleWidget(100),
+        ),
+      ],
+    );
+  }
+}
