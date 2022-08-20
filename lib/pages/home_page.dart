1fe1d6bc4f6520d4b6578436cc7d9fecafada5ab
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-08-20 08:55:57 -0500

    Mods 1.6

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,4 @@
-
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 
@@ -7,6 +7,33 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+
+    double height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          //Fondo
+
+          Positioned(
+            top: - height * 0.18,
+            left: - height * 0.05,
+            child: Transform.rotate(
+              angle: pi / 4,
+              child: Container(
+                height: height * 0.6,
+                width: height * 0.6,
+                decoration: BoxDecoration(
+                  color: Colors.redAccent,
+                  borderRadius: BorderRadius.circular(height * 0.17),
+                ),
+              ),
+            ),
+          ),
+
+          //Contenido principal
+        ],
+      ),
+    );
   }
 }
