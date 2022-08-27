31006a1223f369c801104f8d917f88abde73d7db
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-08-26 22:47:51 -0500

    Mods 4.1

--- lib/pages/news_page.dart
+++ lib/pages/news_page.dart
@@ -1,4 +1,4 @@
-
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo_alerta/models/news_model.dart';
 import 'package:flutter_codigo_alerta/services/api_service.dart';
@@ -25,14 +25,31 @@ class NewsPage extends StatelessWidget {
             List<NewsModel> news = snap.data;
             return news.isNotEmpty
                 ? ListView.builder(
-              itemCount: news.length,
-              itemBuilder: (BuildContext context, int index) {
-                return ItemListWidget(
-                  title: news[index].titulo,
-                  subtitle: news[index].fecha.toString(),
-                );
-              },
-            )
+                    itemCount: news.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        margin: const EdgeInsets.symmetric(
+                            vertical: 12.0, horizontal: 16.0),
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                        ),
+                        width: double.infinity,
+                        height: 260.0,
+                        child: CachedNetworkImage(
+                          imageUrl: "https://images.pexels.com/photos/12791156/pexels-photo-12791156.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                          placeholder: (context, url) => CircularProgressIndicator(),
+                          errorWidget: (context, url, error) => Icon(Icons.error),
+                        ),
+                        // child: Image.network(
+                        //   "http://alertahunter.herokuapp.com/media/Noticias/image_picker1822915258180595565_compressed7606707498878403981.jpg",
+                        //   errorBuilder: (context, error, stackTrace){
+                        //     return Image.asset('assets/images/box.png');
+                        //   },
+                        // ),
+
+                      );
+                    },
+                  )
                 : emptyDataWidget();
           }
           return loadingWidget();
