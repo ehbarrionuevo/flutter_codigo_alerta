9d12a0365e235e2c94634d826b85ca5f7cffe167
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-08-26 20:18:25 -0500

    Mods 2.5

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -42,16 +42,27 @@ class APIService {
     }
   }
 
-  getCitizens() async {
-    String path = "$pathProduction/ciudadanos/";
-    Uri url = Uri.parse(path);
-    http.Response response = await http.get(url);
-    if (response.statusCode == 200) {
-      Map<String, dynamic> myMap = json.decode(response.body);
-      List<CitizenModel> citizenList = myMap["data"].map<CitizenModel>((e)=> CitizenModel.fromJson(e)).toList();
-      print(citizenList);
-      return citizenList;
+  Future<List<CitizenModel>> getCitizens() async {
+    try {
+      String path = "$pathProduction/ciudadanos/";
+      Uri url = Uri.parse(path);
+      http.Response response = await http.get(url);
+      if (response.statusCode == 200) {
+        Map<String, dynamic> myMap = json.decode(response.body);
+        List<CitizenModel> citizenList = myMap["data"]
+            .map<CitizenModel>((e) => CitizenModel.fromJson(e))
+            .toList();
+        return citizenList;
+      }
+      return [];
+    } on TimeoutException catch (error) {
+      return Future.error(
+          "Hubo un inconveniente con servicio, inténtalo nuevamente.");
+    } on SocketException catch (error) {
+      return Future.error(
+          "Hubo un inconveniente con el interner, inténtalo nuevamente.");
+    } on Error catch (error) {
+      return Future.error("Hubo un inconveniente, inténtalo nuevamente.");
     }
-    return [];
   }
 }
