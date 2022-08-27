78bc9b8d541819e721105791bbd697a4fcb7dcc1
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-08-27 09:37:35 -0500

    Mods 4.5

--- lib/pages/news_page.dart
+++ lib/pages/news_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter_codigo_alerta/services/api_service.dart';
 import 'package:flutter_codigo_alerta/ui/general/colors.dart';
 import 'package:flutter_codigo_alerta/ui/widgets/general_widget.dart';
 import 'package:flutter_codigo_alerta/ui/widgets/item_list_widget.dart';
+import 'package:flutter_codigo_alerta/ui/widgets/item_news_widget.dart';
 import 'package:flutter_codigo_alerta/ui/widgets/my_appbar_widget.dart';
 
 class NewsPage extends StatelessWidget {
@@ -28,98 +29,8 @@ class NewsPage extends StatelessWidget {
                 ? ListView.builder(
                     itemCount: news.length,
                     itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        margin: const EdgeInsets.symmetric(
-                            vertical: 40.0, horizontal: 16.0),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(14.0),
-                        ),
-                        width: double.infinity,
-                        height: 240.0,
-                        child: Stack(
-                          clipBehavior: Clip.none,
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(14.0),
-                              child: CachedNetworkImage(
-                                width: double.infinity,
-                                height: 240.0,
-                                imageUrl:
-                                    "http://alertahunter.herokuapp.com/media/Noticias/image_picker1822915258180595565_compressed7606707498878403981.jpg",
-                                fit: BoxFit.cover,
-                                placeholder: (context, url) => loadingWidget(),
-                                errorWidget: (context, url, error) =>
-                                    Image.asset(
-                                  'assets/images/error.jpg',
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
-                            Positioned.fill(
-                              bottom: -40,
-                              child: Align(
-                                alignment: Alignment.bottomCenter,
-                                child: Container(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 16.0, vertical: 14.0),
-                                  margin: const EdgeInsets.symmetric(
-                                      horizontal: 26.0),
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(14.0),
-                                    boxShadow: [
-                                      BoxShadow(
-                                          color:
-                                              Colors.black87.withOpacity(0.05),
-                                          offset: const Offset(0, 5),
-                                          blurRadius: 12.0),
-                                    ],
-                                  ),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Text(
-                                        "12/12/2022",
-                                        style: TextStyle(
-                                          color: kFontPrimaryColor
-                                              .withOpacity(0.75),
-                                          fontSize: 12.0,
-                                        ),
-                                      ),
-                                      divider3,
-                                      Text(
-                                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
-                                        style: TextStyle(
-                                          color: kFontPrimaryColor
-                                              .withOpacity(0.85),
-                                          fontSize: 14.0,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Positioned(
-                              top: 10,
-                              right: 10,
-                              child: Container(
-                                padding: EdgeInsets.all(6.0),
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.white.withOpacity(0.50),
-                                ),
-                                child: Icon(
-                                  Icons.link,
-                                  color: kFontPrimaryColor.withOpacity(0.75),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
+                      return ItemNewsWidget(
+                        newsModel: news[index],
                       );
                     },
                   )
