6c137860476e9efe0d822cefb39fd014f81b9649
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-08-31 21:27:21 -0500

    PDF 2

--- lib/pages/pdf_page.dart
+++ lib/pages/pdf_page.dart
@@ -1,10 +1,30 @@
+import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo_alerta/ui/widgets/button_normal_widget.dart';
 import 'package:flutter_codigo_alerta/ui/widgets/my_appbar_widget.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
 
 class PdfPage extends StatelessWidget {
-  const PdfPage({Key? key}) : super(key: key);
+
+  savePdf() async{
+    pw.Document pdf = pw.Document();
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context){
+          return pw.Text("Hola");
+        }
+      ),
+    );
+    Uint8List bytes = await pdf.save();
+    Directory directory = await getApplicationDocumentsDirectory();
+    File filePdf = File("${directory.path}/alerta.pdf");
+    filePdf.writeAsBytes(bytes);
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +39,8 @@ class PdfPage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: ButtonNormalWidget(
-            onPressed: (){
-
+            onPressed: () {
+              savePdf();
             },
             text: "Generar PDF",
           ),
