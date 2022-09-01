07222ad18862adcfd028ec9f3b382e3bc182bf5f
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-08-31 21:44:32 -0500

    PDF 2.1

--- lib/pages/pdf_page.dart
+++ lib/pages/pdf_page.dart
@@ -4,26 +4,57 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo_alerta/ui/widgets/button_normal_widget.dart';
 import 'package:flutter_codigo_alerta/ui/widgets/my_appbar_widget.dart';
+import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 
 class PdfPage extends StatelessWidget {
-
-  savePdf() async{
+  savePdf() async {
     pw.Document pdf = pw.Document();
     pdf.addPage(
-      pw.Page(
-        build: (pw.Context context){
-          return pw.Text("Hola");
-        }
+      // pw.Page(
+      //   pageFormat: PdfPageFormat.a4,
+      //   build: (pw.Context context){
+      //     return pw.Text("Hola");
+      //   }
+      // ),
+      pw.MultiPage(
+        pageFormat: PdfPageFormat.a4,
+        build: (pw.Context context) {
+          return [
+            pw.Row(
+              children: [
+                pw.Column(
+                  children: [
+                    pw.Text("Elemento: 1",),
+                    pw.Text("Elemento: 1",),
+                    pw.Text("Elemento: 1",),
+                    pw.Text("Elemento: 1",),
+                  ]
+                ),
+              ],
+            ),
+            pw.Divider(),
+            pw.ListView(
+              children: List.generate(
+                200,
+                (index) => pw.Container(
+                  child: pw.Text(
+                    "Hola",
+                  ),
+                ),
+              ),
+            ),
+          ];
+        },
       ),
     );
     Uint8List bytes = await pdf.save();
     Directory directory = await getApplicationDocumentsDirectory();
     File filePdf = File("${directory.path}/alerta.pdf");
     filePdf.writeAsBytes(bytes);
-
+    OpenFile.open(filePdf.path);
   }
 
   @override
